8f4011e464132ab18ab7ff7d25ddd32a98a30235
David Houghton <dhoughton@complyoninc.com>
2012-04-18 09:21:41 -0400

    added some more details to the grammar

--- src/dfh/treepath/PathGrammar.java
+++ src/dfh/treepath/PathGrammar.java
@@ -1,5 +1,8 @@
 package dfh.treepath;
 
+import java.util.regex.Pattern;
+
+import dfh.grammar.Condition;
 import dfh.grammar.Grammar;
 
 /**
@@ -11,8 +14,29 @@ import dfh.grammar.Grammar;
  */
 public class PathGrammar {
 	public static String[] rules = {
-		//
-		"path = <step>++",//
+			//
+			"ROOT = <path> [ '|' <path> ]*",//
+			"path = <step>++",//
+			"step = <forward> <predicate>*+",//
+			"forward = '/'{,2} <name> | '.'{1,2}",//
+			"name = <wildcard> | <specific> | <pattern>",//
+			"wildcard = '*'",//
+			"specific = /(?:\\w|\\\\.)++/",//
+			"pattern = /~(?:[^~\\\\]|\\\\.)++~/ (compiles)",//
+			"attribute = /@[\\p{L}_$][\\p{L}_$\\p{N}]*+/",//
 	};
 	public static Grammar g = new Grammar(rules);
+	static {
+		g.defineCondition("compiles", new Condition() {
+			@Override
+			public boolean passes(CharSequence s) {
+				try {
+					Pattern.compile(s.subSequence(1, s.length() - 1).toString());
+					return true;
+				} catch (Exception e) {
+					return false;
+				}
+			}
+		});
+	}
 }
